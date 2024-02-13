8f2b0e1f0a6cae80683309c83e1576d5c0ca5d2f
Deinerrv <deiner.ra10@gmail.com>
2024-02-13 12:51:13 -0600

    <feat> Routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'auth',
+        loadChildren: () => import('./modules/auth/auth.routes').then(m => m.AUTH_ROUTES)
+    },
+    {
+        path: 'lodgings',
+        loadChildren: () => import('./modules/lodging/lodging.routes').then(m => m.LODGING_ROUTES)
+    },
+    {
+        path: '',
+        loadComponent: () => import('./modules/home/home.component').then(m => m.HomeComponent)
+    },
+];
